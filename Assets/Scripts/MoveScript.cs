6907a6691ad9a7913608a876642e47fb40f56d60
Al20vortex <69950594+Al20vortex@users.noreply.github.com>
2024-09-29 23:13:01 -0700

    First commit, created basic template for VR project.

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Script responsible for moving the player camera (TODO the camera offset?)
+*/
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
